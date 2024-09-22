f3181d86d16ecebbb1225c13259783a075f94ce2
JKLM32 <joefr.gonzals@gmail.com>
2024-09-22 18:56:21 +0800

    Add project files.

---
+++ joefer gonzales/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Label1 = New Label()
+        PictureBox3 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.BackgroundImage = My.Resources.Resources.original
+        PictureBox1.Location = New Point(545, 74)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(327, 402)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.Image = My.Resources.Resources.Daco_5636682
+        PictureBox2.Location = New Point(0, 12)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(310, 102)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(318, 415)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(138, 41)
+        Label1.TabIndex = 2
+        Label1.Text = "message"
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackColor = Color.Transparent
+        PictureBox3.BackgroundImage = My.Resources.Resources.original
+        PictureBox3.Location = New Point(514, 74)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(327, 402)
+        PictureBox3.TabIndex = 0
+        PictureBox3.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.PeachPuff
+        BackgroundImage = My.Resources.Resources.Ocean1
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(872, 476)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox1)
+        Controls.Add(PictureBox2)
+        DoubleBuffered = True
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+
+End Class
---
+++ joefer gonzales/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Private random As New Random()
+    Private moveDirection As Integer = 5 ' Pixels to move per timer tick
+    Private timer As New Timer()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set up a timer for moving PictureBox2
+        timer.Interval = 50 ' Adjust this value to change speed
+        AddHandler timer.Tick, AddressOf MoveTextBox2
+        timer.Start()
+    End Sub
+
+    Private Sub MoveTextBox2(sender As Object, e As EventArgs)
+        ' Move PictureBox2 horizontally
+        PictureBox2.Left += moveDirection
+
+        ' Make PictureBox2 disappear when it reaches the right edge
+        If PictureBox2.Left >= Me.ClientSize.Width Then
+            PictureBox2.Visible = False
+            timer.Stop()
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click, PictureBox3.Click
+        ChangeLabel1Text()
+    End Sub
+
+    Private Sub ChangeLabel1Text()
+        Dim messages() As String = {
+            "Hello there!",
+            "look its a plane!",
+            "Hits a bird!"
+        }
+
+        Dim randomIndex As Integer = random.Next(0, messages.Length)
+        Label1.Text = messages(randomIndex)
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
